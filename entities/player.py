1b818d004bffd06f297e48bc85c2421183a3f864
HenriHTTP <zegordoplays123@gmail.com>
2025-02-04 23:43:43 -0300

    add: player

---
+++ entities/player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, player_name: str, character_race: str, year_old: int, nationality: str, money: int, body_type: str):
+        self.player_name = player_name
+        self.character_race = character_race
+        self.year_old = year_old
+        self.nationality = nationality
+        self.money = money
+        self.body_type = body_type
