4d1c25afb191de03dfd8fe350d2ffdb56a5a5776
HenriHTTP <zegordoplays123@gmail.com>
2025-02-04 23:41:35 -0300

    add:  main

---
+++ main.py
@@ -0,0 +1,21 @@
+import disnake
+from disnake.ext import commands
+import os
+import asyncio
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv('TOKEN')
+intents = disnake.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+# async def load_cogs():
+#     for filename in os.listdir("./cogs"):
+#         if filename.endswith(".py"):
+#             bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+# asyncio.run(load_cogs())
+# bot.run("")
