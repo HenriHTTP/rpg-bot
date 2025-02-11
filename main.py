0cd3145f9a79f4b32dd786391bc9f838e54a0373
HenriHTTP <zegordoplays123@gmail.com>
2025-02-10 21:00:48 -0300

    feat:  add token in main mod

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 
-# async def load_cogs():
-#     for filename in os.listdir("./cogs"):
-#         if filename.endswith(".py"):
-#             bot.load_extension(f"cogs.{filename[:-3]}")
+async def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
 
-
-# asyncio.run(load_cogs())
-# bot.run("")
+asyncio.run(load_cogs())
+bot.run(DISCORD_TOKEN)
