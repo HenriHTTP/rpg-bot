351aa00cc54bf38673c4914c697572e588f71c52
HenriHTTP <zegordoplays123@gmail.com>
2025-02-10 21:01:39 -0300

    feat:  implement creata gettings  class

---
+++ cogs/gretings.py
@@ -0,0 +1,29 @@
+from disnake.ext import commands
+from disnake.ext.commands import Bot
+from services.mensage_service import MessageService
+
+
+
+class Greetings(commands.Cog):
+    def __init__(self, bot : Bot):
+        self.__bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        messages_send_on_server = f'message from:  {ctx.author} content: {ctx.content}'
+        print(messages_send_on_server)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        on_member_join_message = f"Welcome to the server, {member.name} We're glad to have you here"
+        await member.send(on_member_join_message)
+
+    @commands.slash_command(name="hello", description='greetings to user.')
+    async def greet_user(self, ctx):
+        greet_user_message = f'hello, {ctx.author} how are you my friend, be welcome to {ctx.guild}'
+        message = MessageService(ctx, greet_user_message, self.__bot)
+        await message.send_message_user()
+
+
+def setup(bot):
+    bot.add_cog(Greetings(bot))
